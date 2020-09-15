38307b7c340d96ce052928b7c17d3ed082c9c75a
leunoia <68248433+leunoia@users.noreply.github.com>
2020-09-15 01:01:51 -0400

    List Function
    created first version of main function that will create list/pages for each person in the array.

--- js/script.js
+++ js/script.js
@@ -2,8 +2,53 @@
 Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
+function pageMaker (list, page) {
+   const startIndex = (page  * 9) - 9;
+   const endIndex = page * 9;
+   const ul = document.querySelector('.student-list');
+   ul.innerHTML = '';
+   for (let i = 0; i < list.length; i++){
+      if ( i >= startIndex  && i < endIndex){
+         const li = document.createElement('li');
+         li.className = 'student-item cf';
+         ul.appendChild(li);
 
+         const divDetails = document.createElement ('div');
+         divDetails.className = 'student-details';
+         li.appendChild(divDetails);
+         
+         const img = document.createElement('img');
+         img.className = 'avatar';
+         img.alt = 'Profile Picture';
+         const linkSrc = Object.values(data[i])[3];
+         img.src = Object.values(linkSrc)[0];
+         divDetails.insertAdjacentElement ("afterbegin", img);
+         
+         const spanDet = document.createElement('span');
+         spanDet.className = 'email';
+         const dataEmail = Object.values(data[i])[1];
+         spanDet.textContent = dataEmail;
+         divDetails.insertAdjacentElement("beforeend", spanDet)
 
+         const h3 = document.createElement('h3');
+         const name = Object.values(data[i])[0];
+         h3.textContent = `${Object.values(name)[1]} ${Object.values(name)[2]}`;
+         spanDet.insertAdjacentElement("beforebegin",h3);
+
+         const divJoin = document.createElement('div');
+         divJoin.className = 'joined-details';
+         li.appendChild(divJoin);
+
+         const spanJoin = document.createElement('span');
+         spanJoin.className = 'date';
+         const date = Object.values(data[i])[2];
+         spanJoin.innerHTML = `Joined: ${Object.values(date)[0]}`
+         divJoin.appendChild(spanJoin)
+      }
+   }
+
+}
+pageMaker (data, 4)
 
 /*
 For assistance:
